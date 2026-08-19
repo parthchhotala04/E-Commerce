174f40ef530f46bc2964109385b0a097822d7848
Parth Chhotala <parthchhotala014@gmail.com>
2026-08-19 11:49:23 +0530

    Delete src directory

--- src/Admin/AdminDashboard.jsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from "react-router-dom";
-
-export default function AdminDashboard(){
-    return(
-        <>
-       <h1>Admin Dashboard</h1>
-        </>
-    )
-}
--- src/Pages/NoPage.jsx
+++
@@ -1,5 +0,0 @@
-export default function NoPage(){
-    return(
-        <h1>No Page Found</h1>
-    )
-}
